4b17b5fc9231020908edddc9d0ee2f66b0d23ae9
R1013-T <takahashi.ryunosuke.job@gmail.com>
2024-12-29 14:54:08 +0900

    Initial Commit

---
+++ swift-tutorial-weather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-tutorial-weather
+//
+//  Created by 髙橋龍之介 on 2024/12/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-tutorial-weather/swift_tutorial_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_tutorial_weatherApp.swift
+//  swift-tutorial-weather
+//
+//  Created by 髙橋龍之介 on 2024/12/29.
+//
+
+import SwiftUI
+
+@main
+struct swift_tutorial_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
